30b451ca2ef8561abf6469848573dcecf155d0f1
Mount-Blanc <mountblanc87@gmail.com>
2023-02-01 13:22:17 -0600

    made a footer component

---
+++ ecommerce/src/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div >
+      <h1 style={{ color: "white", textAlign: "center", paddingTop: "200px" }}>Main Product or Promotion</h1>
+    </div>
+  );
+};
+
+export default Banner;
