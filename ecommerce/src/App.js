9fe4edbf90b34b2b7fbbe2ce01248a0a5be4ff22
Mount-Blanc <mountblanc87@gmail.com>
2023-02-08 10:54:33 -0600

    deleted unused files

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Testimonials from './components/Testimonials';
-import Footer from './Footer'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
