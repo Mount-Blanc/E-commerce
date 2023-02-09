813324856039ec90512bc6189e086c1e9b34d4ab
Mount-Blanc <mountblanc87@gmail.com>
2023-02-09 10:23:28 -0600

    added validaton to cart badge

--- ecommerce/src/components/CartBadge.js
+++ ecommerce/src/components/CartBadge.js
@@ -3,6 +3,10 @@ import  CartContext  from "./context/cartcontext";
 
 const CartBadge = () => {
   const { cart } = useContext(CartContext);
+
+  if (cart.length === 0) {
+    return 
+  }
   return <div>{cart.length}</div>;
 };
 
